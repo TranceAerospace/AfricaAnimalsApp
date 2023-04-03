79db4b7888224cd26c3891f5e5f87a09d56dfbdf
Trance <xp46290@gmail.com>
2023-04-03 07:37:04 -0400

    Initial project setup up with assets and tab view

---
+++ Africa/App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Mark Trance on 4/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Content")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
