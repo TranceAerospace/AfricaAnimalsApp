af9427762a0f16ddd4de083dbc2f3d2457a9c346
Trance <xp46290@gmail.com>
2023-04-03 22:25:45 -0400

    Built model for parsing JSON and add in Cover Image

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Mark Trance on 4/3/23.
+//
+
+import Foundation
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
