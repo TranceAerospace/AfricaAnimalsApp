a478ecd357c114154a048c32adfc131e185baceb
Trance <xp46290@gmail.com>
2023-04-07 18:19:14 -0400

    Add a video player to play videos from

---
+++ Africa/View/VideoPlayerView.swift
@@ -0,0 +1,45 @@
+//
+//  VideoPlayerView.swift
+//  Africa
+//
+//  Created by Mark Trance on 4/7/23.
+//
+
+import SwiftUI
+import AVKit
+
+struct VideoPlayerView: View {
+    
+    var videoSelected: String
+    var videoTitle: String
+    
+    var body: some View {
+        VStack {
+            VideoPlayer(player: playVideo(fileName: videoSelected, fileFormat: "mp4")) {
+                
+               // Text(videoTitle)
+            }
+            .overlay(
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 32, height: 32)
+                .padding(.top, 6)
+                .padding(.horizontal, 8)
+            , alignment: .topLeading
+            
+            )
+        } // VStack
+        .tint(.accentColor)
+        .navigationTitle(videoTitle)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct VideoPlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            VideoPlayerView(videoSelected: "lion", videoTitle: "Lion")
+        }
+    }
+}
