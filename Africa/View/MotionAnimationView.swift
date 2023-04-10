40386164201163a6bf443274cbbca6e2cd232e9d
Trance <xp46290@gmail.com>
2023-04-10 06:38:20 -0400

    Create motion animation background and implement it to the Gallery View

---
+++ Africa/View/MotionAnimationView.swift
@@ -0,0 +1,69 @@
+//
+//  MotionAnimationView.swift
+//  Africa
+//
+//  Created by Mark Trance on 4/9/23.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    
+    @State private var randomCircle = Int.random(in: 12...16)
+    @State private var isAnimating = false
+    
+    // Random Coordinate
+    func randomCoordinate(max: CGFloat) -> CGFloat {
+        return CGFloat.random(in: 0...max)
+    }
+    // Random Size
+    func randomSize() -> CGFloat {
+        return CGFloat(Int.random(in: 10...300))
+    }
+    // Random Scale
+    func randomScale() -> CGFloat {
+        return CGFloat(Double.random(in: 0.1...2.0))
+    }
+    // Random Speed
+    func randomSpeed() -> Double {
+        return Double.random(in: 0.025...1.0)
+    }
+    // Random Delay
+    func randomDelay() -> Double {
+        return Double.random(in: 0...2)
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                ForEach(0...randomCircle, id: \.self) { item in
+                    Circle()
+                        .foregroundColor(.gray)
+                        .opacity(0.15)
+                        .frame(width: randomSize(), height: randomSize(), alignment: .center)
+                        .scaleEffect(isAnimating ? randomScale() : 1)
+                        .position(x: randomCoordinate(max: geometry.size.width),
+                                  y: randomCoordinate(max: geometry.size.height))
+                    
+                    // TODO: - Replace this with modern iOS 16+ feature
+                        .animation(
+                            Animation.interpolatingSpring(stiffness: 0.5, damping: 0.5)
+                                .repeatForever()
+                                .speed(randomSpeed())
+                                .delay(randomDelay())
+                        )
+                        .onAppear {
+                            isAnimating = true
+                        }
+                }
+            } // ZStack
+            .drawingGroup()
+        } // geoReader
+    }
+}
+
+struct MotionAnimationView_Previews: PreviewProvider {
+    static var previews: some View {
+        MotionAnimationView()
+    }
+}
