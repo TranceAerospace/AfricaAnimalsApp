74a0cbaa2433106e31380f68675bed54ffe7a0af
Trance <xp46290@gmail.com>
2023-04-09 20:34:32 -0400

    Add Map and annotations to display on screen.

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -6,10 +6,40 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    
+    @State private var region: MKCoordinateRegion = {
+        var mapCoordinates = CLLocationCoordinate2D(latitude: 6.600286, longitude: 16.4377599)
+        var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        
+        var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+        return mapRegion
+    }()
+    
+    let locations: [Location] = Bundle.main.decode("locations.json")
+    
     var body: some View {
-        Text("Map")
+        // Basic Map
+        //Map(coordinateRegion: $region)
+        
+        // Advanced Map
+        Map(coordinateRegion: $region, annotationItems: locations) { item in
+            // Old pin style (static)
+            //MapPin(coordinate: item.location, tint: .accentColor)
+            
+            // New Pin Style (static)
+            //MapMarker(coordinate: item.location, tint: .accentColor)
+            
+            // Custom Annotation
+            MapAnnotation(coordinate: item.location) {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32)
+            } // Annotation
+        }
     }
 }
 
