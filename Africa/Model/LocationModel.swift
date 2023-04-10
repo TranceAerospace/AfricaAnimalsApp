dda8e8ae117d0ce5d4cc0dce32dac4b779592b3e
Trance <xp46290@gmail.com>
2023-04-09 20:32:12 -0400

    Add model to decode Location json

---
+++ Africa/Model/LocationModel.swift
@@ -0,0 +1,21 @@
+//
+//  LocationModel.swift
+//  Africa
+//
+//  Created by Mark Trance on 4/7/23.
+//
+
+import Foundation
+import MapKit
+
+struct Location: Codable, Identifiable {
+    let id: String
+    let name: String
+    let image: String
+    let latitude: Double
+    let longitude: Double
+    
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
